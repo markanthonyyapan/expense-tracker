fc72078279f31ce4bed28921e8a7f9717b8e99b4
markanthonyyapan <markanthony.yapan@evsu.edu.ph>
2026-02-08 16:39:27 +0800

    Fix PWA config

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,4 @@ export default withPWA({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  runtimeCaching: {
-    urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|svg|gif|webp)$/,
-    handler: "CacheFirst",
-    options: {
-      cacheName: "images",
-      expiration: {
-        maxEntries: 100,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-      },
-    },
-  },
 })(nextConfig);
